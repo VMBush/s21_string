f26ee9721478ea9f418d068f66c54e598f615e04
Andrey Nesterov <andrejnesterov@MacBook-Air-Andrey.local>
2023-11-07 02:04:56 +0300

    s21_strspn

---
+++ src/s21_strspn.c
@@ -0,0 +1,15 @@
+#include "s21_string.h"
+
+// Вычисления длины начального сегмента строки str1, который состоит только из
+// символов, присутствующих в строке str2.
+
+size_t s21_strspn(const char *str1, const char *str2) {
+  s21_size_t count = 0;
+
+  while (*str1 && s21_strchr(str2, *str1)) {
+    count++;
+    str1++;
+  }
+
+  return count;  // Возвращаем длину начального сегмента
+}
