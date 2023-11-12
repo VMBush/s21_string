73483794abb3fd854b7360632e1bc9d278d48538
Anna Feklina <ann05.feklina@yandex.ru>
2023-11-12 23:10:25 +0300

    +s21_to_lower

--- src/s21_string.h
+++ src/s21_string.h
@@ -31,8 +31,8 @@ char *s21_strtok(char *str, const char *delim);
 
 int s21_sprintf(char *str, const char *format, ...);
 
-void *s21_to_upper(const char *string);
-void *s21_to_lower(const char *string);
+void *s21_to_upper(const char *str);
+void *s21_to_lower(const char *str);
 void *s21_insert(const char *src, const char *str, s21_size_t start_index);
 void *s21_trim(const char *src, const char *trim_chars);
 
---
+++ src/s21_to_lower.c
@@ -0,0 +1,16 @@
+#include "s21_string.h"
+
+void *s21_to_lower(const char *str){
+  char *res = s21_NULL;
+  if (str) {
+    res = malloc((s21_strlen(str) + 1) * sizeof(char));
+    if (res) {
+        s21_strcpy(res, str);
+        for (s21_size_t i = 0; i < s21_strlen(str); i++)
+            if (res[i] >= 'A' && res[i] <= 'Z'){
+                res[i] = res[i] + 32;
+            }
+    }
+  }
+  return res;
+}
